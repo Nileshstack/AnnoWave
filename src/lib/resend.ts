fc21f97573e3f70eeaaf60b9739e747990bd2142
nilesh <nileshsrivastava.stjoseph@gmail.com>
2026-01-30 22:43:04 +0530

    Updated resend.ts

--- src/lib/resend.ts
+++ src/lib/resend.ts
@@ -1,4 +1,4 @@
-//Not in use as resend is replaced by Nodemailer...
-/*import { Resend } from 'resend';
+/*Not in use as resend is replaced by Nodemailer...
+import { Resend } from 'resend';
 
- export const resend = new Resend(process.env.RESEND_API_KEY);
+ export const resend = new Resend(process.env.RESEND_API_KEY);*/
